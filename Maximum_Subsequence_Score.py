a8eda5591d5b61ee4406c0be64939cc08cc11134
Nikhil Kumar Jha <78644716+nik-6174@users.noreply.github.com>
2023-07-28 15:13:20 +0530

    Create Maximum_Subsequence_Score.py

---
+++ Maximum_Subsequence_Score.py
@@ -0,0 +1,16 @@
+# Title: 2542. Maximum Subsequence Score
+# Difficulty: Medium
+# Problem: https://leetcode.com/problems/maximum-subsequence-score/description/
+
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        nums = sorted(zip(nums2, nums1), reverse=True)
+        q = []
+        ans = s = 0
+        for a, b in nums:
+            s += b
+            heappush(q, b)
+            if len(q) == k:
+                ans = max(ans, s * a)
+                s -= heappop(q)
+        return ans
