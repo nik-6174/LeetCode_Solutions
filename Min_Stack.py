bb980cb4ec609f2586e36e2923a640d79a3c1760
Nikhil Kumar Jha <78644716+nik-6174@users.noreply.github.com>
2023-09-21 22:09:20 +0530

    Create Min_Stack.py

---
+++ Min_Stack.py
@@ -0,0 +1,22 @@
+# Title: 155. Min Stack
+# Difficulty: Medium
+# Problem: https://leetcode.com/problems/min-stack/description/
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
