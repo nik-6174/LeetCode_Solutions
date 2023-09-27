d98bbf7451e8ef72b75bd41a8088a7658fb9d36a
Nikhil Kumar Jha <78644716+nik-6174@users.noreply.github.com>
2023-09-27 22:57:35 +0530

    Create Longest_Common_Subsequence.py

---
+++ Longest_Common_Subsequence.py
@@ -0,0 +1,18 @@
+# Title: 1143. Longest Common Subsequence
+# Difficulty: Medium
+# Problem: https://leetcode.com/problems/longest-common-subsequence/description/
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        # Create a DP table to store the LCS lengths for subproblems
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[m][n]
