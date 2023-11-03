9c7801ee03477d8096eb7b4462d380342a91289b
Nikhil Kumar Jha <78644716+nik-6174@users.noreply.github.com>
2023-11-03 16:37:18 +0530

    Update Word_Break.py

--- Word_Break.py
+++ Word_Break.py
@@ -4,24 +4,15 @@
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = []
-        for i in range(len(s)):
-            if s[:i+1] in wordDict: # if the entire substring is a word in the wordDict
-                dp.append(True)
-            else:
-                # set flag to check if the substring can be formed using the words in wordDict
-                flag = True
-                j = i-1
-                while j >= 0:
-                    # check if any combination of previously formed substrings and a word in the dictionary can form the current substring
-                    if dp[j] and s[j+1:i+1] in wordDict:
-                        dp.append(True)
-                        # set flag to False so we don't append False
-                        flag=False
-                        break
-                    j -= 1
-                # conclude that the substring could not be formed using the words in wordDict
-                if flag:
-                    dp.append(False)
-        # return if the whole string can be formed with the words in the dictionary
-        return dp[-1]
+
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
